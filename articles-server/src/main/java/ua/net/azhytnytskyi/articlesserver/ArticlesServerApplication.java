a8d9bb117a02cc317698c6852637713d5c1c2b28
artemzhytnytskyi <a.zhytnytskyy@gmail.com>
2019-04-20 15:53:05 +0300

    initial commit

---
+++ articles-server/src/main/java/ua/net/azhytnytskyi/articlesserver/ArticlesServerApplication.java
@@ -0,0 +1,13 @@
+package ua.net.azhytnytskyi.articlesserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticlesServerApplication.class, args);
+    }
+
+}
