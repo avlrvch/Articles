1abe8db3e81d387e93687a4f9314004a203d11fa
artemzhytnytskyi <a.zhytnytskyy@gmail.com>
2019-04-21 16:41:45 +0300

    added base cors configs

--- articles-server/src/main/java/ua/net/azhytnytskyi/articlesserver/ArticlesServerApplication.java
+++ articles-server/src/main/java/ua/net/azhytnytskyi/articlesserver/ArticlesServerApplication.java
@@ -2,6 +2,11 @@ package ua.net.azhytnytskyi.articlesserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class ArticlesServerApplication {
@@ -10,4 +15,17 @@ public class ArticlesServerApplication {
         SpringApplication.run(ArticlesServerApplication.class, args);
     }
 
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
 }
